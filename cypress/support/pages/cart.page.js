2c39d7b5652af8f756131e4c42507d00ebc03df5
André Scheffer <andre.scheffer@dbserver.com.br>
2026-02-20 17:12:20 -0300

    Retirado comentàrio

--- cypress/support/pages/cart.page.js
+++ cypress/support/pages/cart.page.js
@@ -1,5 +1,5 @@
 export const cartPage = {
   checkoutBtn: '[data-test="checkout"]',
   cartItem: '.cart_item',
-  removeButton: '[data-test^="remove-"]' // Seletor dinâmico para qualquer botão de remover no carrinho
+  removeButton: '[data-test^="remove-"]'
 }
